d1fbce0ad5ecce2647a956beeb05114709796dd6
Alberto Tudela <ajtudela@gmail.com>
2024-09-13 20:09:00 +0200

    Update dependencies
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 # Please make sure that bin/bash is the location of your bash terminal,if not please replace with your local machine's bash path
 branch="${1:-development}"
+ROS_DISTRO="humble"
 echo "Installing dependencies for branch $branch"
 sudo DEBIAN_FRONTEND=noninteractive apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends  \
     build-essential \
@@ -20,35 +21,30 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get update && sudo DEBIAN_FRONTEND=nonin
 echo "Installing third-party software cppitertools"
 sudo git clone https://github.com/ryanhaining/cppitertools /usr/local/include/cppitertools
 cd /usr/local/include/cppitertools
-sudo mkdir build
-cd build
-sudo cmake ..
-sudo make install
+sudo mkdir -p build && cd build
+sudo cmake .. && sudo make install
 
 echo "Installing third-party software Fast-RTPS"
-
-mkdir -p ~/software
-cd ~/software
-git clone --branch 1.0.x https://github.com/eProsima/Fast-CDR.git
-mkdir Fast-CDR/build
-cd Fast-CDR/build
-export MAKEFLAGS=-j$(($(grep -c ^processor /proc/cpuinfo) - 0))
-cmake ..
-cmake --build .
-sudo make install
-cd ~/software
-git clone https://github.com/eProsima/foonathan_memory_vendor.git
-cd foonathan_memory_vendor
-mkdir build
-cd build
-cmake ..
-cmake --build .
-sudo make install
-cd ~/software
-git clone --branch 2.6.8 https://github.com/eProsima/Fast-DDS.git
-mkdir Fast-DDS/build
-cd Fast-DDS/build
-cmake ..
-cmake --build .
-sudo make install
-sudo ldconfig
+if [ -d "/opt/ros/${ROS_DISTRO}" ]; then
+  echo -e "fastRTPS is already installed. Skipping installation."
+  source /opt/ros/${ROS_DISTRO}/setup.bash
+else
+  mkdir -p ~/software
+  cd ~/software
+  git clone --branch 1.0.x https://github.com/eProsima/Fast-CDR.git
+  cd Fast-CDR
+  mkdir -p build && cd build
+  export MAKEFLAGS=-j$(($(grep -c ^processor /proc/cpuinfo) - 0))
+  cmake .. && cmake --build . && sudo make install
+  cd ~/software
+  git clone https://github.com/eProsima/foonathan_memory_vendor.git
+  cd foonathan_memory_vendor
+  mkdir -p build && cd build
+  cmake .. && cmake --build . && sudo make install
+  cd ~/software
+  git clone --branch 2.6.8 https://github.com/eProsima/Fast-DDS.git
+  cd Fast-DDS
+  mkdir -p build && cd build
+  cmake .. && cmake --build . && sudo make install
+  sudo ldconfig
+fi
